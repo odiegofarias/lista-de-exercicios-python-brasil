dc65e407d08414fd5d5057163540bd6ab6c71130
Diego Farias <diego@exemplo.com>
2022-05-30 21:12:35 -0300

    Corrigindo os arquivos 1 a 3 da seção02 estrutura de decisão

--- secao_02_estrutura_de_decisao/ex_03_F_ou_M.py
+++ secao_02_estrutura_de_decisao/ex_03_F_ou_M.py
@@ -18,3 +18,9 @@ Retorne: F - Feminino ou M - Masculino. Para quaisquer outros valores, retorne S
 
 def f_ou_m(sexo):
     """Escreva aqui em baixo a sua solução"""
+    if sexo == 'F':
+        return 'F - Feminino'
+    elif sexo == 'M':
+        return 'M - Masculino'
+    else:
+        return 'Sexo inválido'
